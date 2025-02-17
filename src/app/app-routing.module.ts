770844acb34a371f7c764c319458a7e733468f0d
Martin <Martin@testmail.com>
2025-02-18 00:26:12 +0100

    created the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './pages/home/home.component';
 import { BookingComponent } from './pages/booking/booking.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default Route
   { path: 'booking', component: BookingComponent },
-  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'admin', component: AdminDashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown paths to Home
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
